7c3a7b59b7cf9e07cc3bb787a170d5ee5bc5b272
Endurance Ogbutor <eddyfree69@gmail.com>
2022-09-16 22:26:05 +0100

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,9 +34,11 @@ const NavBar = () => {
     return (
         <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
             <div>
-                <h1 className="text-5xl font-signature ml-2">
-                    <Link to={home}>Vokus</Link>
-                </h1>
+                <Link to={home}>
+                    <h1 className="text-5xl font-signature ml-2">
+                        Vokus
+                    </h1>
+                </Link>   
             </div>
             <ul className="hidden md:flex">
                 {links.map(({id, link}) => (
