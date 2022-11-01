f6c2b8e1a96cc30f9a1078542800c1604c5d6c16
Endurance Ogbutor <eddyfree69@gmail.com>
2022-11-01 07:19:41 +0100

    portfolio-website

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,6 +5,7 @@ import javascript from '../assets/javascript.png';
 import reactimg from '../assets/react.png';
 import tailwindcss from '../assets/tailwind.png';
 import css from '../assets/css.png';
+import { motion, Variants } from "framer-motion";
 
 const Experience = () => {
 
@@ -45,7 +46,18 @@ const Experience = () => {
         title: 'CSS',
         style: 'shadow-blue-500'
       }
-    ]
+  ];
+  const CardAnimate = {
+    offscreen: {
+      translateY: "100px", opacity: 0
+    },
+    onscreen: {
+      translateY: 0, opacity: 1,
+      transition: {
+        type: 'spring', bounce: 0.3, duration: 1
+      }
+    }
+  }
 
   return (
     <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full text-white md:h-screen'>
@@ -58,16 +70,23 @@ const Experience = () => {
             These are the technologies i've worked with
           </p>
         </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+        <motion.div
+          initial={"offscreen"}
+          whileInView={"onscreen"}
+          viewport={{ once: false, amount: 0.2 }}
+          transition={{ staggerChildren: 0.2 }}
+          className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
           {techs.map(({id, src, title, style}) => (
-            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-            <img src={src} alt="" className='w-20 mx-auto'/>
+            <motion.div
+              variants={CardAnimate}
+              key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+              <img src={src} alt="" className='w-20 mx-auto' />
             <p className='mt-4'>
               {title}
             </p>
-          </div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </div>
   )
